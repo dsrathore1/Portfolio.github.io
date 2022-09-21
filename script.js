91819c4efbea4de340cb0966f653f4e268cc7918
DS Rathore <dsrathorebsw1234@gmail.com>
2022-09-21 18:13:25 +0530

    Edited

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Blogger", "Designer", "Photographer"],
+        strings: ["Developer", "Designer", "Photographer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Blogger", "Designer", "Photographer"],
+        strings: ["Developer", "Designer", "Photographer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
